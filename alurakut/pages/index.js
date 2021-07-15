225ad79d0c5687658b2c2bf12103167982df9bfe
thaysrq <thayspereirasilva2@gmail.com>
2021-07-15 18:37:26 -0300

    props

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,13 +1,23 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 
+function ProfileSidebar(props){
+  console.log("githubuser props --->", props)
+  return(
+    <Box>
+       <img  style={{ borderRadius: '8px'}} src={`https://github.com/${props.githubUser}.png`}/>
+    </Box>
+  )
+}
+
 export default function Home() {
+
+  const githubUser = 'thaysrq';
+
   return (
     <MainGrid>
       <div className="profileArea" style={{ gridArea: 'profileArea'}}>
-        <Box>
-          <img src="https://github.com/thaysrq.png"/>
-        </Box>
+        <ProfileSidebar githubUser={githubUser}/>
       </div>
       <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
         <Box>Bem vindo</Box>
